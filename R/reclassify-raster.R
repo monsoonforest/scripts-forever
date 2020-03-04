90ebf7ed901c6cac24a9430d23d391ac9da9fc34
Chintan Sheth <chintz604@gmail.com>
2020-03-04 12:13:35 +0530

    reclassify a raster function

---
+++ R/reclassify-raster.R
@@ -0,0 +1,33 @@
+
+##  NDWI SCRIPT TO MAP GLACIAL LAKES IN ARUNACHAL
+
+packages <- c("raster", "rgdal", "sp", "plotly", "maptools")
+
+lapply(X = packages, FUN = library, character.only=TRUE)
+
+## RASTER RECLASSIFY FUNCTION
+
+rasterlist <- list.files(pattern=".*.*\\.grd$$", recursive=FALSE)
+
+reclassify <- function(x,...){
+
+for (i in 1:length(x)){
+
+r <- raster(paste0(x[i]))
+
+## reclassify matrix 
+						## 0 to   1200 = NA
+						##    1201 to 10000000000 = 1
+						m <- c(0, 1200, NA, 1201, 10000000000, 1)
+
+						## create matrix of the same
+						rclmat <- matrix(m, ncol=3, byrow=TRUE)
+
+					rc <- reclassify(r, rclmat)
+
+writeRaster(rc, filename = "reclassified-raster-rivers.tif", format="GTiff", overwrite=TRUE)
+
+}
+ }
+
+ reclassify(rasterlist)
