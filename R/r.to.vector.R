617e110474e78fd1964aef79f5bc784d040d52b0
Chintan Sheth <chintz604@gmail.com>
2020-03-04 12:12:51 +0530

    fast script for raster to vector conversion

--- R/r.to.vector.R
+++ R/r.to.vector.R
@@ -22,10 +22,10 @@ gdal_polygonizeR <- function(x, outshape=NULL, gdalformat = 'ESRI Shapefile',
   setwd(dirname(pypath))
   if (!is.null(outshape)) {
     outshape <- sub('\\.shp$', '', outshape)
-    f.exists <- file.exists(paste(outshape, c('shp', 'shx', 'dbf'), sep='.'))
+    f.exists <- file.exists(paste(outshape, c('shp', 'shx', 'dbf', 'prj'), sep='.'))
     if (any(f.exists))
       stop(sprintf('File already exists: %s',
-                   toString(paste(outshape, c('shp', 'shx', 'dbf'),
+                   toString(paste(outshape, c('shp', 'shx', 'dbf', 'prj'),
                                   sep='.')[f.exists])), call.=FALSE)
   } else outshape <- tempfile()
   if (is(x, 'Raster')) {
@@ -45,82 +45,82 @@ gdal_polygonizeR <- function(x, outshape=NULL, gdalformat = 'ESRI Shapefile',
 }
 	
 
-rasterToPolygons <- function (x, fun = NULL, n = 4, na.rm = TRUE, digits = 12, dissolve = FALSE) 
-{
-    stopifnot(n %in% c(4, 8, 16))
-    if (nlayers(x) > 1) {
-        if (!is.null(fun)) {
-            stop("you cannot supply a \"fun\" argument when \"x\" has multiple layers")
-        }
-    }
-    if (!fromDisk(x) & !inMemory(x)) {
-        xyv <- xyFromCell(x, 1:ncell(x))
-        xyv <- cbind(xyv, NA)
-    }
-    else if (!(na.rm) | inMemory(x) | canProcessInMemory(x)) {
-        xyv <- cbind(xyFromCell(x, 1:ncell(x)), getValues(x))
-        x <- clearValues(x)
-        if (na.rm) {
-            nas <- apply(xyv[, 3:ncol(xyv), drop = FALSE], 1, 
-                function(x) all(is.na(x)))
-            xyv <- xyv[!nas, , drop = FALSE]
-        }
-        if (!is.null(fun)) {
-            if (nrow(xyv) > 0) {
-                xyv <- subset(xyv, fun(xyv[, 3]))
-            }
-        }
-    }
-    else {
-        tr <- blockSize(x)
-        xyv <- matrix(ncol = 3, nrow = 0)
-        nl <- nlayers(x)
-        for (i in 1:tr$n) {
-            start <- cellFromRowCol(x, tr$row[i], 1)
-            end <- start + tr$nrows[i] * ncol(x) - 1
-            xyvr <- cbind(xyFromCell(x, start:end), getValues(x, 
-                row = tr$row[i], nrows = tr$nrows[i]))
-            if (na.rm) {
-                if (nl > 1) {
-                  nas <- apply(xyvr[, 3:ncol(xyvr), drop = FALSE], 
-                    1, function(x) all(is.na(x)))
-                }
-                else {
-                  nas <- is.na(xyvr[, 3])
-                }
-                xyvr <- xyvr[!nas, , drop = FALSE]
-            }
-            if (nrow(xyvr) > 0) {
-                if (!is.null(fun)) {
-                  xyvr <- subset(xyvr, fun(xyvr[, 3, drop = FALSE]))
-                }
-                rownames(xyvr) <- NULL
-                xyv <- rbind(xyv, xyvr)
-            }
-        }
-    }
-    colnames(xyv) <- c("x", "y", names(x))
-    if (nrow(xyv) == 0) {
-        warning("no values in selection")
-        return(NULL)
-    }
-    cr <- .getPolygons(xyv[, 1:2, drop = FALSE], res(x), n)
-    cr <- round(cr, digits = digits)
-    sp <- lapply(1:nrow(cr), function(i) Polygons(list(Polygon(matrix(cr[i, 
-        ], ncol = 2))), i))
-    sp <- SpatialPolygons(sp, proj4string = crs(x))
-    sp <- SpatialPolygonsDataFrame(sp, data.frame(xyv[, 3:ncol(xyv), 
-        drop = FALSE]), match.ID = FALSE)
-    if (dissolve) {
-        if (!requireNamespace("rgeos")) {
-            warning("package rgeos is not available. Cannot dissolve")
-        }
-        else {
-            sp <- aggregate(sp, names(sp))
-        }
-    }
-    sp
-}
+# rasterToPolygons <- function (x, fun = NULL, n = 4, na.rm = TRUE, digits = 12, dissolve = FALSE) 
+# {
+#     stopifnot(n %in% c(4, 8, 16))
+#     if (nlayers(x) > 1) {
+#         if (!is.null(fun)) {
+#             stop("you cannot supply a \"fun\" argument when \"x\" has multiple layers")
+#         }
+#     }
+#     if (!fromDisk(x) & !inMemory(x)) {
+#         xyv <- xyFromCell(x, 1:ncell(x))
+#         xyv <- cbind(xyv, NA)
+#     }
+#     else if (!(na.rm) | inMemory(x) | canProcessInMemory(x)) {
+#         xyv <- cbind(xyFromCell(x, 1:ncell(x)), getValues(x))
+#         x <- clearValues(x)
+#         if (na.rm) {
+#             nas <- apply(xyv[, 3:ncol(xyv), drop = FALSE], 1, 
+#                 function(x) all(is.na(x)))
+#             xyv <- xyv[!nas, , drop = FALSE]
+#         }
+#         if (!is.null(fun)) {
+#             if (nrow(xyv) > 0) {
+#                 xyv <- subset(xyv, fun(xyv[, 3]))
+#             }
+#         }
+#     }
+#     else {
+#         tr <- blockSize(x)
+#         xyv <- matrix(ncol = 3, nrow = 0)
+#         nl <- nlayers(x)
+#         for (i in 1:tr$n) {
+#             start <- cellFromRowCol(x, tr$row[i], 1)
+#             end <- start + tr$nrows[i] * ncol(x) - 1
+#             xyvr <- cbind(xyFromCell(x, start:end), getValues(x, 
+#                 row = tr$row[i], nrows = tr$nrows[i]))
+#             if (na.rm) {
+#                 if (nl > 1) {
+#                   nas <- apply(xyvr[, 3:ncol(xyvr), drop = FALSE], 
+#                     1, function(x) all(is.na(x)))
+#                 }
+#                 else {
+#                   nas <- is.na(xyvr[, 3])
+#                 }
+#                 xyvr <- xyvr[!nas, , drop = FALSE]
+#             }
+#             if (nrow(xyvr) > 0) {
+#                 if (!is.null(fun)) {
+#                   xyvr <- subset(xyvr, fun(xyvr[, 3, drop = FALSE]))
+#                 }
+#                 rownames(xyvr) <- NULL
+#                 xyv <- rbind(xyv, xyvr)
+#             }
+#         }
+#     }
+#     colnames(xyv) <- c("x", "y", names(x))
+#     if (nrow(xyv) == 0) {
+#         warning("no values in selection")
+#         return(NULL)
+#     }
+#     cr <- .getPolygons(xyv[, 1:2, drop = FALSE], res(x), n)
+#     cr <- round(cr, digits = digits)
+#     sp <- lapply(1:nrow(cr), function(i) Polygons(list(Polygon(matrix(cr[i, 
+#         ], ncol = 2))), i))
+#     sp <- SpatialPolygons(sp, proj4string = crs(x))
+#     sp <- SpatialPolygonsDataFrame(sp, data.frame(xyv[, 3:ncol(xyv), 
+#         drop = FALSE]), match.ID = FALSE)
+#     if (dissolve) {
+#         if (!requireNamespace("rgeos")) {
+#             warning("package rgeos is not available. Cannot dissolve")
+#         }
+#         else {
+#             sp <- aggregate(sp, names(sp))
+#         }
+#     }
+#     sp
+# }
 
 ## for loop
 convertraster <- function(x,...){
@@ -134,17 +134,17 @@ polyall <-	gdal_polygonizeR(r, outshape=NULL, gdalformat = 'ESRI Shapefile',
 poly <- polyall[polyall$DN==1,]
 
 ## ASSIGN PROJECTION
-proj4string(poly) <- "+proj=utm +zone=46 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+##proj4string(poly) <- "+proj=utm +zone=46 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
 ## CHANGE FIELD NAME
 names(poly) <- "value"
 
 filepath <- paste0("poly_", unlist(strsplit(basename(x[i]),"\\."))[1])
 
-writePolyShape(poly,filepath)	
+writePolyShape(poly,filename=filepath)	
 
 }
 
 }
-
-convertraster(lakes)
+	
+convertraster(lakes)	
